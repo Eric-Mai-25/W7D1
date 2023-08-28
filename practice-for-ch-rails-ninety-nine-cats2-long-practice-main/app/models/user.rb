cade412df6d9ed684bf7a073ed197cc90e835b12
Eric-Mai-25 <ericmai2@gmail.com>
2023-08-28 14:40:22 -0700

    created user model and most methods

---
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/models/user.rb
@@ -0,0 +1,53 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+class User < ApplicationRecord
+    before_validation :ensure_session_token
+    validates :session_token, :username, presence: true, uniqueness: true
+
+
+
+
+    def password=(password)
+        self.password_digest = Bcrypt::Password.create(password)
+        @password = password
+    end
+
+    def is_password?(password)
+       password_obj = BCrypt::Password.new(self.password_digest) 
+       password_obj.is_password?(password) 
+    end
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        if user && user.is_password?(password)
+            user
+        else
+            nil
+        end
+    end
+
+    
+
+    private
+
+    def generate_unique_session_token
+        while true
+            session_token = SecureRandom.urlsafe_base64
+            return session_token unless User.exists?(session_token: session_token)
+        end
+        
+    end
+
+    def ensure_session_token
+        self.session_token ||= generate_unique_session_token
+    end
+end
